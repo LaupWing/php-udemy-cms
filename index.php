a525492f93824d2a4fbf497f6d20119a971f861c
LaupWing <loc_strict@hotmail.com>
2023-03-03 11:06:37 +0100

    update

--- index.php
+++ index.php
@@ -1,32 +1,6 @@
 <?php include "./includes/header.php" ?>
-   <!-- Navigation -->
-   <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-      <div class="container">
-         <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-               <span class="sr-only">Toggle navigation</span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-               <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Start Bootstrap</a>
-         </div>
-         <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-               <li>
-                  <a href="#">About</a>
-               </li>
-               <li>
-                  <a href="#">Services</a>
-               </li>
-               <li>
-                  <a href="#">Contact</a>
-               </li>
-            </ul>
-         </div>
-      </div>
-   </nav>
-
+<!-- Navigation -->
+<?php include "./includes/navigation.php" ?>
    <!-- Page Content -->
    <div class="container">
 
@@ -163,25 +137,5 @@
       <!-- /.row -->
 
       <hr>
-
-      <!-- Footer -->
-      <footer>
-         <div class="row">
-            <div class="col-lg-12">
-               <p>Copyright &copy; Your Website 2014</p>
-            </div>
-            <!-- /.col-lg-12 -->
-         </div>
-         <!-- /.row -->
-      </footer>
-
-   </div>
-   <!-- /.container -->
-
-   <!-- jQuery -->
-   <script src="js/jquery.js"></script>
-
-   <!-- Bootstrap Core JavaScript -->
-   <script src="js/bootstrap.min.js"></script>
 
 <?php include "./includes/footer.php" ?>
