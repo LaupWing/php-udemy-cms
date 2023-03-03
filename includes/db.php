bca49eec03e7c166e602cecee8fdc402579bba86
LaupWing <loc_strict@hotmail.com>
2023-03-03 11:17:47 +0100

    UPdate

--- includes/db.php
+++ includes/db.php
@@ -5,8 +5,7 @@
    define("DB_NAME", "cms");
 
    $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-   
-   if($connection){
-      echo "We are connected";
-   }
+   // if($connection){
+   //    echo "We are connected";
+   // }
 ?>
--- includes/navigation.php
+++ includes/navigation.php
@@ -11,6 +11,15 @@
       </div>
       <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
          <ul class="nav navbar-nav">
+            <?php 
+               $query = "SELECT * FROM categories";
+               $select_all_categories_query = mysqli_query($connection, $query);
+               while($row = mysqli_fetch_assoc($select_all_categories_query)){
+                  $category_title = $row["category_title"];
+                  echo "<li> <a href='#'> {$category_title} </a> </li>";
+               }
+            ?>
+
             <li>
                <a href="#">About</a>
             </li>
