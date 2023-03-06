568c36328a20b8ed87ba571124b019ce437d9e81
LaupWing <loc_strict@hotmail.com>
2023-03-06 08:51:05 +0100

    udpate

--- admin/index.php
+++ admin/index.php
@@ -8,8 +8,8 @@
             <div class="row">
                <div class="col-lg-12">
                   <h1 class="page-header">
-                     Blank Page
-                     <small>Subheading</small>
+                     Welcom to admin
+                     <small>Author</small>
                   </h1>
                </div>
             </div>
