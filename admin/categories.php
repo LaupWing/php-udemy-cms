cf0df5d1f0fb3aff9369f071869fb05e14c60e9d
LaupWing <loc_strict@hotmail.com>
2023-03-06 09:16:55 +0100

    103

--- admin/categories.php
+++ admin/categories.php
@@ -12,7 +12,25 @@
                      <small>Author</small>
                   </h1>
                   <div class="col-xs-6">
-                     <form action="">
+                     <?php 
+                        if(isset($_POST["submit"])){
+                           $category_title = $_POST["category_title"];
+
+                           if($category_title == "" || empty($category_title)){
+                              echo "This field should not be empty";
+                           } else {
+                              $query = "INSERT INTO categories(category_title)";
+                              $query .= "VALUES('{$category_title}')";
+
+                              $create_category_query = mysqli_query($connection, $query);
+
+                              if(!$create_category_query){
+                                 die("QUERY FAILED". mysqli_error($connection));
+                              }
+                           }
+                        }
+                     ?>
+                     <form action="" method="post">
                         <div class="form-group">
                            <label for="category-title">Add Category</label>
                            <input class="form-control" type="text" name="category_title">
