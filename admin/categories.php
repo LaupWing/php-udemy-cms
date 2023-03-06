a556d920904c25a4a386e01253c558320bc20f20
LaupWing <loc_strict@hotmail.com>
2023-03-06 09:06:44 +0100

    101

--- admin/categories.php
+++ admin/categories.php
@@ -22,6 +22,22 @@
                         </div>
                      </form>
                   </div>
+                  <div class="col-xs-6">
+                     <table class="table table-bordered table-hover">
+                        <thead>
+                           <tr>
+                              <th>Id</th>
+                              <th>Category Title</th>
+                           </tr>
+                        </thead>
+                        <tbody>
+                           <tr>
+                              <td>Baseball category</td>
+                              <td>Baseball category</td>
+                           </tr>
+                        </tbody>
+                     </table>
+                  </div>
                </div>
             </div>
             <!-- /.row -->
