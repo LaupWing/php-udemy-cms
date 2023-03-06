1727439f4ecbcbd8b3a3d96c754eaf6e5e8af743
LaupWing <loc_strict@hotmail.com>
2023-03-06 09:41:12 +0100

    update

--- admin/categories.php
+++ admin/categories.php
@@ -39,6 +39,26 @@
                            <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                         </div>
                      </form>
+                     <form action="" method="post">
+                        <div class="form-group">
+                           <label for="category-title">Edit Category</label>
+                           <?php 
+                              if(isset($_GET["edit"])){
+                                 $category_id = $_GET["edit"];
+                                 $query = "SELECT * FROM categories WHERE category_id = $category_id";
+                                 $select_categories_id = mysqli_query($connection, $query);
+                                 while($row = mysqli_fetch_assoc($select_categories_id)){
+                                    $category_id = $row["category_id"];
+                                    $category_title = $row["category_title"];
+                              }
+                           ?>
+                              <input value="<?php if(isset($cate)) ?>" class="form-control" type="text" name="category_title">
+                           <?php } ?>
+                        </div>
+                        <div class="form-group">
+                           <input class="btn btn-primary" type="submit" name="submit" value="Update Category">
+                        </div>
+                     </form>
                   </div>
                   <div class="col-xs-6">
                      <?php 
@@ -62,6 +82,7 @@
                                  echo "<td>{$category_id}</td>";
                                  echo "<td>{$category_title}</td>";
                                  echo "<td><a href='categories.php?delete={$category_id}'>Delete</a></td>";
+                                 echo "<td><a href='categories.php?edit={$category_id}'>Edit</a></td>";
                                  echo "</tr>";
                               }
                            ?>
