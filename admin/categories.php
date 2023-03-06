c109e43c439e5cec214bd4f7bb32e250267abd7c
LaupWing <loc_strict@hotmail.com>
2023-03-06 09:03:32 +0100

    100

--- admin/categories.php
+++ admin/categories.php
@@ -1,7 +1,9 @@
 <?php include "includes/header.php";?>
    <?php include "includes/navigation.php"; ?>
       <div id="page-wrapper">
+
          <div class="container-fluid">
+
             <!-- Page Heading -->
             <div class="row">
                <div class="col-lg-12">
@@ -9,6 +11,17 @@
                      Welcom to admin
                      <small>Author</small>
                   </h1>
+                  <div class="col-xs-6">
+                     <form action="">
+                        <div class="form-group">
+                           <label for="category-title">Add Category</label>
+                           <input class="form-control" type="text" name="category_title">
+                        </div>
+                        <div class="form-group">
+                           <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                        </div>
+                     </form>
+                  </div>
                </div>
             </div>
             <!-- /.row -->
