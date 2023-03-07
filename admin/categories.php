9b679ef84d2f1e15982f2de6b71ad78e612af1fc
LaupWing <loc_strict@hotmail.com>
2023-03-07 08:06:21 +0100

    109

--- admin/categories.php
+++ admin/categories.php
@@ -41,28 +41,8 @@
                            </tr>
                         </thead>
                         <tbody>
-                           <?php 
-                              $query = "SELECT * FROM categories";
-                              $select_categories = mysqli_query($connection, $query);
-                              while($row = mysqli_fetch_assoc($select_categories)){
-                                 $category_id = $row["category_id"];
-                                 $category_title = $row["category_title"];
-                                 echo "<tr>";
-                                 echo "<td>{$category_id}</td>";
-                                 echo "<td>{$category_title}</td>";
-                                 echo "<td><a href='categories.php?delete={$category_id}'>Delete</a></td>";
-                                 echo "<td><a href='categories.php?edit={$category_id}'>Edit</a></td>";
-                                 echo "</tr>";
-                              }
-                           ?>
-                           <?php 
-                              if(isset($_GET["delete"])){
-                                 $delete_category_id = $_GET["delete"];
-                                 $query = "DELETE FROM categories WHERE category_id = {$delete_category_id}";
-                                 $delete_query = mysqli_query($connection, $query);
-                                 header("Location: categories.php");
-                              }
-                           ?>
+                           <?php findAllCategories();?>
+                           <?php deleteCategories();?>
                         </tbody>
                      </table>
                   </div>
--- admin/functions.php
+++ admin/functions.php
@@ -18,4 +18,31 @@ function insert_categories(){
       }
    }
 }
+
+
+function findAllCategories() {
+   global $connection;
+   $query = "SELECT * FROM categories";
+   $select_categories = mysqli_query($connection, $query);
+   while($row = mysqli_fetch_assoc($select_categories)){
+      $category_id = $row["category_id"];
+      $category_title = $row["category_title"];
+      echo "<tr>";
+      echo "<td>{$category_id}</td>";
+      echo "<td>{$category_title}</td>";
+      echo "<td><a href='categories.php?delete={$category_id}'>Delete</a></td>";
+      echo "<td><a href='categories.php?edit={$category_id}'>Edit</a></td>";
+      echo "</tr>";
+   }
+}
+
+function deleteCategories(){
+   global $connection;
+   if(isset($_GET["delete"])){
+      $delete_category_id = $_GET["delete"];
+      $query = "DELETE FROM categories WHERE category_id = {$delete_category_id}";
+      $delete_query = mysqli_query($connection, $query);
+      header("Location: categories.php");
+   }
+}
 ?>
