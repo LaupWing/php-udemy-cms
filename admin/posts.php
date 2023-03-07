50df31b8bd7c2a8dca260eac8eb4e5c7ea4f187e
LaupWing <loc_strict@hotmail.com>
2023-03-07 08:20:59 +0100

    111

--- admin/posts.php
+++ admin/posts.php
@@ -39,17 +39,20 @@
                               $post_tags = $row["post_tags"];
                               $post_comment_count = $row["post_comment_count"];
                               $post_date = $row["post_date"];
+
+                              echo "<tr>";
+                              echo "<td>{$post_id}</td>";
+                              echo "<td>{$post_title}</td>";
+                              echo "<td>{$post_author}</td>";
+                              echo "<td>{$post_category_id}</td>";
+                              echo "<td>{$post_status}</td>";
+                              echo "<td><img width='100' src='../images/{$post_image}'/></td>";
+                              echo "<td>{$post_tags}</td>";
+                              echo "<td>{$post_comment_count}</td>";
+                              echo "<td>{$post_date}</td>";
+                              echo "</tr>";
                            }
                         ?>
-                           <td>Id</td>
-                           <td>Author</td>
-                           <td>Title</td>
-                           <td>Category</td>
-                           <td>Status</td>
-                           <td>Image</td>
-                           <td>Tags</td>
-                           <td>Comments</td>
-                           <td>Date</td>
                         
                      </tbody>
                   </table>
