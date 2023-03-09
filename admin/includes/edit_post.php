2bce31705546c8659b11f0f8381c4e32b3e62abc
LaupWing <loc_strict@hotmail.com>
2023-03-09 08:25:58 +0100

    update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -3,7 +3,7 @@
       $post_id = $_GET["p_id"];
    }
 
-   $query = "SELECT * FROM posts";
+   $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
    $select_posts_by_id = mysqli_query($connection, $query);
    while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
       $post_id = $row["post_id"];
@@ -17,6 +17,34 @@
       $post_comment_count = $row["post_comment_count"];
       $post_date = $row["post_date"];
    }
+
+   if(isset($_POST["update_post"])){
+      $post_author = $_POST["post_author"];
+      $post_title = $_POST["post_title"];
+      $post_category_id = $_POST["post_category"];
+      $post_status = $_POST["post_status"];
+      $post_image = $_FILES["image"]["name"];
+      $post_image_temp = $_FILES["image"]["tmp_name"];
+      $post_content = $_POST["post_content"];
+      $post_tags = $_POST["post_tags"];
+
+      move_uploaded_file($post_image_temp, "../images/$post_image");
+
+      $query = "UPDATE posts SET ";
+      $query .= "post_title = '{$post_title}', ";
+      $query = "post_category_id = '{$post_category_id}', ";
+      $query = "post_date = now(), ";
+      $query = "post_author = '{$post_author}', ";
+      $query = "post_status = '{$post_status}', ";
+      $query = "post_tags = '{$post_tags}', ";
+      $query = "post_content = '{$post_content}', ";
+      $query = "post_image = '{$post_image}' ";
+      $query = "WHERE post_id = {$post_id} ";
+
+      $update_post = mysqli_query($connection, $query);
+
+      confirm($update_post);
+   }
 ?>
 
 
@@ -27,7 +55,7 @@
       <input type="text" name="title" value="<?php echo $post_title; ?>" class="form-control">
    </div>
    <div class="form-group">
-      <select name="" id="">
+      <select name="post_category" id="">
          <?php 
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
