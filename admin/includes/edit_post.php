19187c0b1774ab8c720a3a86e77f2b7eae065329
LaupWing <loc_strict@hotmail.com>
2023-03-09 08:39:03 +0100

    update

--- admin/includes/edit_post.php
+++ admin/includes/edit_post.php
@@ -19,8 +19,8 @@
    }
 
    if(isset($_POST["update_post"])){
-      $post_author = $_POST["post_author"];
-      $post_title = $_POST["post_title"];
+      $post_author = $_POST["author"];
+      $post_title = $_POST["title"];
       $post_category_id = $_POST["post_category"];
       $post_status = $_POST["post_status"];
       $post_image = $_FILES["image"]["name"];
@@ -37,20 +37,21 @@
             $post_image = $row["post_image"];
          }
       }
-
+      echo $post_image;
+      echo $post_id;
+      echo $post_id;
       $query = "UPDATE posts SET ";
       $query .= "post_title = '{$post_title}', ";
-      $query = "post_category_id = '{$post_category_id}', ";
-      $query = "post_date = now(), ";
-      $query = "post_author = '{$post_author}', ";
-      $query = "post_status = '{$post_status}', ";
-      $query = "post_tags = '{$post_tags}', ";
-      $query = "post_content = '{$post_content}', ";
-      $query = "post_image = '{$post_image}' ";
-      $query = "WHERE post_id = {$post_id} ";
+      $query .= "post_category_id = '{$post_category_id}', ";
+      $query .= "post_date = now(), ";
+      // $query .= "post_author = '{$post_author}', ";
+      // $query .= "post_status = '{$post_status}', ";
+      // $query .= "post_tags = '{$post_tags}', ";
+      // $query .= "post_content = '{$post_content}', ";
+      // $query .= "post_image = '{$post_image}' ";
+      $query .= "WHERE post_id = {$post_id} ";
 
       $update_post = mysqli_query($connection, $query);
-
       confirm($update_post);
    }
 ?>
@@ -84,7 +85,7 @@
       <input type="text" name="post_category_id" value="<?php echo $post_category_id; ?>" class="form-control">
    </div>
    <div class="form-group">
-      <label for="title">Post Author</label>
+      <label for="author">Post Author</label>
       <input type="text" value="<?php echo $post_author; ?>" name="author" class="form-control">
    </div>
    <div class="form-group">
@@ -94,6 +95,7 @@
    <div class="form-group">
       <label for="post_image">Post Image</label>
       <img width="100" src="../images/<?php echo $post_image; ?>" alt="">
+      <input type="file" name="image">
    </div>
    <div class="form-group">
       <label for="post_tags">Post Tags</label>
@@ -105,6 +107,6 @@
    </div>
 
    <div class="form-group">
-      <input type="submit" class="btn btn-primary" name="create_post" value="Publish Post">
+      <input type="submit" class="btn btn-primary" name="update_post" value="Publish Post">
    </div>
 </form>
