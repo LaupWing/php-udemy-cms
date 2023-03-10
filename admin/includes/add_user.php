b325752bbaa859d909e73bbae17db2b0d579f23a
LaupWing <loc_strict@hotmail.com>
2023-03-10 09:19:13 +0100

    145

--- admin/includes/add_user.php
+++ admin/includes/add_user.php
@@ -1,15 +1,15 @@
 <?php 
-   if(isset($_POST["create_post"])){
-      $post_title = $_POST["title"];
-      $post_author = $_POST["author"];
-      $post_category_id = $_POST["post_category"];
-      $post_status = $_POST["post_status"];
-      $post_image = $_FILES["image"]["name"];
-      $post_image_temp = $_FILES["image"]["tmp_name"];
+   if(isset($_POST["create_user"])){
+      $user_firstname = $_POST["user_firstname"];
+      $user_lastname = $_POST["user_lastname"];
+      $user_email = $_POST["user_email"];
+      $user_role = $_POST["user_role"];
+      // $post_image = $_FILES["image"]["name"];
+      // $post_image_temp = $_FILES["image"]["tmp_name"];
 
-      $post_tags = $_POST["post_tags"];
-      $post_content = $_POST["post_content"];
-      $post_date = date("d-m-y");
+      $username = $_POST["username"];
+      $user_password = $_POST["user_password"];
+      // $post_date = date("d-m-y");
 
       move_uploaded_file($post_image_temp, "../images/{$post_image}");
 
@@ -32,19 +32,9 @@
       <input type="text" name="user_lastname" class="form-control">
    </div>
    <select name="user_role" id="">
-      <?php 
-         $query = "SELECT * FROM users";
-         $select_users = mysqli_query($connection, $query);
-
-         confirm($select_users);
-
-         while($row = mysqli_fetch_assoc($select_users)){
-            $user_id = $row["user_id"];
-            $user_role = $row["user_role"];
-            echo "<option value='{$user_id}'>{$user_role}</option>";
-         }
-      ?>
-      
+      <option value="subscriber">Select options</option>      
+      <option value="admin">Admin</option>      
+      <option value="subscriber">Subscriber</option>      
    </select>
    
    <!-- <div class="form-group">
