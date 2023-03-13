7051b2de7c882ee9a2c62c27a4f04cfc3c684f9f
LaupWing <loc_strict@hotmail.com>
2023-03-13 09:05:45 +0100

    161

--- includes/login.php
+++ includes/login.php
@@ -24,17 +24,14 @@
          $db_user_role = $row["user_role"];
       }
 
-      if($username !== $db_username && $password !== $db_user_password){
-         header("Location: ../index.php");
-      } else if ($username == $db_username && $password == $db_user_password){
-         
+      if($username === $db_username && $password === $db_user_password){
          $_SESSION["username"] = $db_username;
          $_SESSION["user_firstname"] = $db_user_firstname;
          $_SESSION["user_lastname"] = $db_user_lastname;
          $_SESSION["user_role"] = $db_user_role;
-
+         
          header("Location: ../admin.php");
-      } else {
+      }else {
          header("Location: ../index.php");
       }
    }
