6117d781a307f7bf218a935bc4171e7a5a69414e
LaupWing <loc_strict@hotmail.com>
2023-03-13 08:57:50 +0100

    udpate

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -3,6 +3,16 @@
 <?php include "functions.php"; ?>
 <?php session_start(); ?>
 
+<?php 
+
+if(isset($_SESSION["user_role"])) {
+   if($_SESSION["user_role"] !== "admin"){
+      header("Location: ../index.php");
+   }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
