212d66647235ccc2c6e6d742b0bc9c3e89bebb80
LaupWing <loc_strict@hotmail.com>
2023-03-15 09:03:59 +0100

    198

--- registration.php
+++ registration.php
@@ -17,6 +17,10 @@
       if(!$select_randsalt_query){
          die("Query Failed". mysqli_error($connection));
       }
+
+      while($row = mysqli_fetch_array($select_randsalt_query)){
+         
+      }
    }
 ?>
 
