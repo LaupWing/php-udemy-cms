81b5cacff185abdc76f133da61e1f6c218d31ee1
LaupWing <loc_strict@hotmail.com>
2023-03-15 08:16:26 +0100

    187

--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -3,3 +3,17 @@ $(document).ready(function () {
       height: 200
    })
 })
+
+$(document).ready(function(){
+   $("#selectAllBoxes").click(function(event){
+      if(this.checked){
+         $(".checkboxes").each(function(){
+            this.checked = true
+         })
+      }else{
+         $(".checkboxes").each(function(){
+            this.checked = false
+         })
+      }
+   })
+})
