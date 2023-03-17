830d68c889873e601e838c5b070a2190ecef8473
LaupWing <loc_strict@hotmail.com>
2023-03-17 11:17:14 +0100

    224

--- admin/includes/admin_navigation.php
+++ admin/includes/admin_navigation.php
@@ -12,6 +12,7 @@
    </div>
    <!-- Top Menu Items -->
    <ul class="nav navbar-right top-nav">
+      <li><a href="">Users Online <?php users_online(); ?></a></li>
       <li><a href="../index.php">Home</a></li>
       <li class="dropdown">
          <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-user"></i> 
