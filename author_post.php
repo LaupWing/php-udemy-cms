58d8c012ea9909c0710c45a23a3d66890b2f0221
LaupWing <loc_strict@hotmail.com>
2023-03-17 10:00:22 +0100

    update

--- author_post.php
+++ author_post.php
@@ -12,10 +12,10 @@
             <?php 
                if(isset($_GET["p_id"])){
                   $post_id = $_GET["p_id"];
-                  $post_author = $_GET["post_author"];
+                  $post_author = $_GET["author"];
                }
 
-               $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
+               $query = "SELECT * FROM posts WHERE post_author = '{$post_author}'";
                $select_all_posts_query = mysqli_query($connection, $query);
                while($row = mysqli_fetch_assoc($select_all_posts_query)){
                   $post_title = $row["post_title"];
@@ -68,59 +68,8 @@
                   }else{
                      echo "<script>alert('fields cannot be empty')</script>";
                   }
-
-
                }
             ?>
-         <!-- Comments Form -->
-            <div class="well">
-               <h4>Leave a Comment:</h4>
-               <form action="" role="form" method="post">
-                  <div class="form-group">
-                     <label for="Author">Author</label>
-                     <input class="form-control" type="text" name="comment_author">
-                  </div>
-                  <div class="form-group">
-                     <label for="Email">Email</label>
-                     <input class="form-control" type="text" name="comment_email">
-                  </div>
-                  <div class="form-group">
-                     <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                  </div>
-                  <button name="create_comment" type="submit" class="btn btn-primary">Submit</button>
-               </form>
-            </div>
-
-            <hr>
-
-            <!-- Posted Comments -->
-            <?php
-               $query = "SELECT * FROM comments WHERE comment_post_id = {$post_id} ";
-               $query .= "AND comment_status = 'approved' ";
-               $query .= "ORDER BY comment_id DESC ";
-               $select_comment_query = mysqli_query($connection, $query);
-               if(!$select_comment_query){
-                  die("Query Failed". mysqli_error($connection));
-               }
-
-               while($row = mysqli_fetch_array(($select_comment_query))){
-                  $comment_date = $row["comment_date"];
-                  $comment_content = $row["comment_content"];
-                  $comment_author = $row["comment_author"];
-               
-            ?>
-               <div class="media">
-                  <a class="pull-left" href="#">
-                     <img class="media-object" src="http://placehold.it/64x64" alt="">
-                  </a>
-                  <div class="media-body">
-                     <h4 class="media-heading"><?php echo $comment_author ?>
-                        <small><?php echo $comment_date ?></small>
-                     </h4>
-                     <?php echo $comment_content ?>
-                  </div>
-               </div>
-            <?php } ?>
             <hr>
          </div>
          <?php include "./includes/sidebar.php" ?>
