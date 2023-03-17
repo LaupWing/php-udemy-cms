0b2c7bb0b2ab4d4e3720dd6343871982820c4610
LaupWing <loc_strict@hotmail.com>
2023-03-17 10:24:44 +0100

    213

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -75,6 +75,7 @@
             <th>View</th>
             <th>Edit</th>
             <th>Delete</th>
+            <th>Count</th>
          </tr>
       </thead>
       <tbody>
@@ -91,6 +92,7 @@
                $post_tags = $row["post_tags"];
                $post_comment_count = $row["post_comment_count"];
                $post_date = $row["post_date"];
+               $post_views_count = $row["post_views_count"];
    
                echo "<tr>";
                echo "<td> <input class='checkboxes' type='checkbox' name='checkBoxArray[]' value='{$post_id}'></td>";
@@ -111,6 +113,7 @@
                echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?')\" href='posts.php?delete={$post_id}'>Delete</a></td>";
+               echo "<td>{$post_views_count}</td>";
                echo "</tr>";
             }
          ?>
