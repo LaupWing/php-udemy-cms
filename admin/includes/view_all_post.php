6cd1a98ac09bbd3f37b8289055bca9e43d2155c0
LaupWing <loc_strict@hotmail.com>
2023-03-17 10:12:55 +0100

    UDPATE

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -5,22 +5,40 @@
 
          switch($bulk_options){
             case "published":
-
                $query = "UPDATE posts SET post_status = 'published'  WHERE post_id = {$post_id}";
                $update_published_status = mysqli_query($connection, $query);
                confirm($update_published_status);
                break;
             case "draft":
-
                $query = "UPDATE posts SET post_status = 'draft'  WHERE post_id = {$post_id}";
                $update_draft_status = mysqli_query($connection, $query);
                confirm($update_draft_status);
                break;
             case "delete":
-
                $query = "DELETE FROM posts WHERE post_id = {$post_id}";
                $update_delete_status = mysqli_query($connection, $query);
                confirm($update_delete_status);
+               break;
+            case "clone":
+               $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
+               $select_post_query = mysqli_query($connection, $query);
+               while($row = mysqli_fetch_array($select_post_query)){
+                  $post_title = $row["post_title"];
+                  $post_category_id = $row["post_category_id"];
+                  $post_date = $row["post_date"];
+                  $post_author = $row["post_author"];
+                  $post_status = $row["post_status"];
+                  $post_image = $row["post_image"];
+                  $post_tags = $row["post_tags"];
+                  $post_content = $row["post_content"];
+               }
+               $query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tags, post_status)";
+               $quyer .= "VALUES({$post_category_id}, '{$post_title}','{$post_author}', now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}')";
+
+               $copy_query = mysqli_query($connection, $query);
+               if(!$copy_query){
+                  die("QUERY FAILED". mysqli_error($connection));
+               }
                break;
          }
       }
@@ -35,6 +53,7 @@
             <option value="published">Publish</option>
             <option value="draft">Draft</option>
             <option value="delete">Delete</option>
+            <option value="clonse">Clone</option>
          </select>
       </div>
       <div class="col-xs-4">
@@ -60,7 +79,7 @@
       </thead>
       <tbody>
          <?php 
-            $query = "SELECT * FROM posts";
+            $query = "SELECT * FROM posts ORDER BY post_id DESC";
             $select_posts = mysqli_query($connection, $query);
             while($row = mysqli_fetch_assoc($select_posts)){
                $post_id = $row["post_id"];
