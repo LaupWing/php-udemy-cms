66226819f50233ab35d908a150ee929918a3b13e
LaupWing <loc_strict@hotmail.com>
2023-03-17 10:28:53 +0100

    udpate

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -113,7 +113,7 @@
                echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete?')\" href='posts.php?delete={$post_id}'>Delete</a></td>";
-               echo "<td>{$post_views_count}</td>";
+               echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                echo "</tr>";
             }
          ?>
@@ -122,6 +122,12 @@
                $post_id = $_GET["delete"];
                $query = "DELETE FROM posts where post_id = {$post_id}";
                $delete_query = mysqli_query($connection, $query);
+               header("Location: post.php");
+            }
+            if(isset($_GET["rest"])){
+               $post_id = $_GET["reset"];
+               $query = "UPDATE posts SET post_views_count = 0 where post_id = {$post_id}";
+               $reset_query = mysqli_query($connection, $query);
                header("Location: post.php");
             }
          ?>
