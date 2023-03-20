d7dabd1f806c4552e3c6a7fef6fd358cacd3a4b6
LaupWing <loc_strict@hotmail.com>
2023-03-20 09:16:45 +0100

    235

--- admin/includes/view_all_post.php
+++ admin/includes/view_all_post.php
@@ -113,7 +113,7 @@
                $row = mysqli_fetch_array($send_comment_query);
                $comment_id = $row["comment_id"];
                $count_comments = mysqli_num_rows($send_comment_query);
-               echo "<td><a href='comment.php?id={$comment_id}'>{$count_comments}</a></td>";
+               echo "<td><a href='post_comment.php?id={$post_id}'>{$count_comments}</a></td>";
                echo "<td>{$post_date}</td>";
                echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
