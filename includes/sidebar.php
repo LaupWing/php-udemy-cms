61a77018ab0ffd7988e23f82800b4a194d7f8dd1
LaupWing <loc_strict@hotmail.com>
2023-03-21 09:17:25 +0100

    264

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -15,29 +15,36 @@
       <!-- /.input-group -->
    </div>
    <div class="well">
-      <h4>Login</h4>
-      <form action="includes/login.php" method="post">
-         <div class="form-group">
-            <input name="username" type="text" class="form-control" placeholder="Enter Username">
-         </div>
-         <div class="input-group">
-            <input name="password" type="password" class="form-control" placeholder="Enter Password">
-            <span class="input-group-btn">
-               <button class="btn btn-primary" name="login" type="submit">
-                  Submit
-               </button>
-            </span>
-         </div>
-      </form>
+      <?php if(isset($_SESSION["user_role"])): ?>
+         <h4>Log in as <?php echo $_SESSION["user_role"]; ?></h4>
+         <a href="includes/logout.php" class="btn btn-primary">Logout</a>
+      <?php else: ?>
+         <h4>Login</h4>
+         <form action="includes/login.php" method="post">
+            <div class="form-group">
+               <input name="username" type="text" class="form-control" placeholder="Enter Username">
+            </div>
+            <div class="input-group">
+               <input name="password" type="password" class="form-control" placeholder="Enter Password">
+               <span class="input-group-btn">
+                  <button class="btn btn-primary" name="login" type="submit">
+                     Submit
+                  </button>
+               </span>
+            </div>
+         </form>
+      <?php endif; ?>
+      <?php 
+         $query = "SELECT * FROM categories";
+         $select_categories_query_sidebar = mysqli_query($connection, $query);
+      ?>
+      
       <!-- /.input-group -->
    </div>
 
    <!-- Blog Categories Well -->
    <div class="well">
-      <?php 
-         $query = "SELECT * FROM categories";
-         $select_categories_query_sidebar = mysqli_query($connection, $query);
-      ?>
+      
       <h4>Blog Categories</h4>
       <div class="row">
          <div class="col-lg-12">
