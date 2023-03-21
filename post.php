277e1fd9ab8691558c6395dfe257ee3b0b852952
LaupWing <loc_strict@hotmail.com>
2023-03-21 08:59:48 +0100

    udpate

--- post.php
+++ post.php
@@ -18,19 +18,27 @@
                   if(!$send_query){
                      die("Query failed");
                   }
-                  $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
+
+                  if(isset($_SESSION["user_role"]) && $_SESSION["user_role"] == "admin"){
+                     $query = "SELECT * FROM posts WHERE post_id = {$post_id}";
+                  } else{
+                     $query = "SELECT * FROM posts WHERE post_id = {$post_id} AND post_status = 'published'";
+                  }
+
                   $select_all_posts_query = mysqli_query($connection, $query);
-                  while($row = mysqli_fetch_assoc($select_all_posts_query)){
-                     $post_title = $row["post_title"];
-                     $post_author = $row["post_author"];
-                     $post_date = $row["post_date"];
-                     $post_image = $row["post_image"];
-                     $post_content = $row["post_content"];
+                  if(mysqli_num_rows($select_all_posts_query) < 1){
+                     echo "<h1 class='text-center'> NO POSTS YET </h1>";
+                  }else {
+                     while($row = mysqli_fetch_assoc($select_all_posts_query)){
+                        $post_title = $row["post_title"];
+                        $post_author = $row["post_author"];
+                        $post_date = $row["post_date"];
+                        $post_image = $row["post_image"];
+                        $post_content = $row["post_content"];
             ?>
 
                <h1 class="page-header">
-                  Page Heading
-                  <small>Secondary Text</small>
+                  Post
                </h1>
                <!-- First Blog Post -->
                <h2>
@@ -49,9 +57,7 @@
 
             <?php
                   }
-               }else {
-                  header("Location: index.php");
-               }
+               
             ?>
             <?php 
                if(isset($_POST["create_comment"])){
@@ -126,7 +132,11 @@
                      <?php echo $comment_content ?>
                   </div>
                </div>
-            <?php } ?>
+            <?php 
+            } } }else {
+               header("Location: index.php");
+            } 
+            ?>
             <hr>
          </div>
          <?php include "./includes/sidebar.php" ?>
