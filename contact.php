a3bd85e0f88b55dbb624701e32c36c28276fdea2
LaupWing <loc_strict@hotmail.com>
2023-03-21 08:06:27 +0100

    251

--- contact.php
+++ contact.php
@@ -57,18 +57,17 @@
                   <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
                      <div class="form-group">
                         <label for="email" class="sr-only">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                        <input type="email" name="email" id="email" class="form-control" placeholder="Enter your email">
                      </div>
                      <div class="form-group">
-                        <label for="email" class="sr-only">Email</label>
-                        <input type="email" name="email" id="email" class="form-control" placeholder="somebody@example.com">
+                        <label for="subject" class="sr-only">Subject</label>
+                        <input type="text" name="subject" id="subject" class="form-control" placeholder="Enter your subject">
                      </div>
                      <div class="form-group">
-                        <label for="password" class="sr-only">Password</label>
-                        <input type="password" name="password" id="key" class="form-control" placeholder="Password">
+                        <textarea class="form-control" name="body" id="body" cols="50" rows="10"></textarea>
                      </div>
 
-                     <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
+                     <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Submit">
                   </form>
 
                </div>
