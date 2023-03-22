f1acd7ae6eef6ef2d94047fc018a3ffc1ad1ebe2
LaupWing <loc_strict@hotmail.com>
2023-03-22 11:20:16 +0100

    272

--- admin/functions.php
+++ admin/functions.php
@@ -113,4 +113,21 @@ function checkUserRole($table, $column, $role) {
    return mysqli_num_rows($result);
 }
 
+function isAdmin($username = ""){
+   global $connection;
+
+   $query = "SELECT user_role FROM users WHERE username = '$username'"; 
+   $result = mysqli_query($connection, $query);
+
+   confirm($result);
+
+   $row = mysqli_fetch_array($result);
+
+   if($row["user_role"] == "admin"){
+      return true;
+   }else{
+      return false;
+   }
+}
+
 ?>
