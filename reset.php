838be9812f6723a9be0d57b1954e35a277786771
LaupWing <loc_strict@hotmail.com>
2023-03-24 08:19:25 +0100

    305

--- reset.php
+++ reset.php
@@ -5,9 +5,8 @@
    // if(!isset($_GET["email"]) && !isset($_GET["token"])){
    //    redirect("index");
    // }
-   $token = "";
    if($stmt = mysqli_prepare($connection, "SELECT username, user_email, token FROM users WHERE token=?")){
-      mysqli_stmt_bind_param($stmt, "s", $token);
+      mysqli_stmt_bind_param($stmt, "s", $_GET["token"]);
       mysqli_stmt_execute($stmt);
       mysqli_stmt_bind_result($stmt, $username, $user_email, $token);
       mysqli_stmt_fetch($stmt);
@@ -22,12 +21,13 @@
          $hashedPassword = password_hash($password, PASSWORD_BCRYPT , array("cost" => 12));
 
          if($stmt = mysqli_prepare($connection, "UPDATE users SET token = '', user_password='{$hashedPassword}' WHERE user_email = ?")){
-            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_bind_param($stmt, "s", $_GET["email"]);
             mysqli_stmt_execute($stmt);
 
             if(mysqli_stmt_affected_rows($stmt) >= 1){
-               
+               redirect("/cms/login.php");
             }
+            mysqli_stmt_close($stmt);
          }
       }
    }
@@ -37,39 +37,37 @@
 
 <!-- Page Content -->
 <div class="container">
-
-   <div class="form-gap"></div>
-   <div class="container">
-      <div class="row">
-         <div class="col-md-4 col-md-offset-4">
-            <div class="panel panel-default">
-               <div class="panel-body">
-                  <div class="text-center">
-                     <h3><i class="fa fa-lock fa-4x"></i></h3>
-                     <h2 class="text-center">Forgot Password?</h2>
-                     <p>You can reset your password here.</p>
-                     <div class="panel-body">
-                        <form id="register-form" role="form" autocomplete="off" class="form" method="post">
-                           <div class="form-group">
-                              <div class="input-group">
-                                 <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
-                                 <input id="email" name="email" placeholder="email address" class="form-control" type="email">
+      <div class="form-gap"></div>
+      <div class="container">
+         <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+               <div class="panel panel-default">
+                  <div class="panel-body">
+                     <div class="text-center">
+                        <h3><i class="fa fa-lock fa-4x"></i></h3>
+                        <h2 class="text-center">Forgot Password?</h2>
+                        <p>You can reset your password here.</p>
+                        <div class="panel-body">
+                           <form id="register-form" role="form" autocomplete="off" class="form" method="post">
+                              <div class="form-group">
+                                 <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-envelope color-blue"></i></span>
+                                    <input id="email" name="email" placeholder="email address" class="form-control" type="email">
+                                 </div>
                               </div>
-                           </div>
-                           <div class="form-group">
-                              <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
-                           </div>
-                           <input type="hidden" class="hide" name="token" id="token" value="">
-                        </form>
-                     </div><!-- Body-->
-                     <h2>Please check your email</h2>
+                              <div class="form-group">
+                                 <input name="recover-submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit">
+                              </div>
+                              <input type="hidden" class="hide" name="token" id="token" value="">
+                           </form>
+                        </div><!-- Body-->
+                        <h2>Please check your email</h2>
+                     </div>
                   </div>
                </div>
             </div>
          </div>
       </div>
-   </div>
-
 
    <hr>
 
