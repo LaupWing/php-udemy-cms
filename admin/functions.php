6e82e7b2d53de239d1ffd8d2fd684ef8bf7e833e
LaupWing <loc_strict@hotmail.com>
2023-03-24 09:27:06 +0100

    319

--- admin/functions.php
+++ admin/functions.php
@@ -52,6 +52,11 @@ function loggedInUserId(){
    return false;
 }
 
+function userLikedPost($post_id = ""){
+   $result = query("SELECT * FROM likes WHERE user_id=". loggedInUserId(). "AND post_id={$post_id}");
+   return mysqli_num_rows($result) >= 1 ? true : false;
+}
+
 function checkIfUserIsLoggedInAndRedirect($redirectLocation){
    if(isLoggedIn()){
       redirect($redirectLocation);
