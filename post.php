0609272e8cc37b34a0b0b344867c271face43ce6
LaupWing <loc_strict@hotmail.com>
2023-03-24 09:02:48 +0100

    314

--- post.php
+++ post.php
@@ -15,6 +15,8 @@
       if(mysqli_num_rows($postResult) >= 1){
          // echo ""
       }
+
+      mysqli_query($connection, "UPDATE posts SET likes=$likes+1 WHERE post_id={$post_id}");
    }
 
 ?>
